e697e8d7a648d7db05d57e7bb442842505c4342f
chetanpatelmobitv <chetan_patel@persistent.com>
2020-05-01 18:01:38 +0530

    rearranged code

--- src/test/java/com/JavaPrograms/legagyProgram/NumberSwap.java
+++ src/test/java/com/JavaPrograms/legagyProgram/NumberSwap.java
@@ -1,4 +1,4 @@
-package com.JavaPrograms;
+package com.JavaPrograms.legagyProgram;
 
 public class NumberSwap {
 
---
+++ src/test/java/com/JavaPrograms/legagyProgram/fromIdea.java
@@ -0,0 +1,4 @@
+package com.JavaPrograms.legagyProgram;
+
+public class fromIdea {
+}
