d836b9cd7b556e93078c89f8d8f201d751ecbd7e
chetanpatelmobitv <chetan_patel@persistent.com>
2020-05-15 23:05:22 +0530

    updated

--- src/test/java/com/JavaPrograms/Array/ArrayFrequency.java
+++ src/test/java/com/JavaPrograms/Array/ArrayFrequency.java
@@ -1,38 +1,27 @@
 package com.JavaPrograms.Array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ArrayFrequency {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 String str = "picture perfect";
-		 
-		 char arr [] = str.toCharArray();
-	        //Array fr will store frequencies of element  
-	        int [] fr = new int [arr.length];  
-	        int visited = -1;  
-	        for(int i = 0; i < arr.length; i++){  
-	            int count = 1;  
-	            for(int j = i+1; j < arr.length; j++){  
-	                if(arr[i] == arr[j]){  
-	                    count++;  
-	                    //To avoid counting same element again  
-	                    fr[j] = visited;  
-	                }  
-	            }  
-	            if(fr[i] != visited)  
-	                fr[i] = count;  
-	        }  
-	  
-	        //Displays the frequency of each element present in array  
-	        System.out.println("---------------------------------------");  
-	        System.out.println(" Element | Frequency");  
-	        System.out.println("---------------------------------------");  
-	        for(int i = 0; i < fr.length; i++){  
-	            if(fr[i] != visited && arr[i] != ' ' )  
-	                System.out.println("    " + arr[i] + "    |    " + fr[i]);  
-	        }  
-	        System.out.println("----------------------------------------"); 
 
+		int arr[] = {21, 3, 4, 5, 2, 8, 6,21,3};
+		HashMap<Integer,Integer> map = new HashMap<>();
+
+		for ( int number : arr) {
+
+			if ( map.containsKey(number)) {
+				map.put(number,map.get(number)+1 );
+			}
+			else {
+				map.put(number,1);
+			}
+		}
+
+		for(Map.Entry<Integer,Integer> map1 : map.entrySet()) {
+			System.out.println(map1.getKey() + " " + map1.getValue());
+		}
 	}
-
 }
